e31367f52cf80de890eae8515cd5fd1694ecf82f
Gustavo Dellacroce <gmdella95@gmail.com>
2020-09-20 03:18:21 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,8 +4,6 @@ def oxford_comma(array)
   elsif array.length > 2
         array.insert(-2, "and")
         array[0..-1].join(", ")
-  elsif array.length == 3      
-        array[-1].insert(0, "and ")
   else
       array.join
   end
