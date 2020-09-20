1e3ce154acf0ecab695d37e6dbf1426363de1f2c
Gustavo Dellacroce <gmdella95@gmail.com>
2020-09-20 03:19:38 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,7 @@ def oxford_comma(array)
   if array.length == 2
         array.join(" and ")
   elsif array.length > 2
+        array[-1].insert(0, "and ")
         array[0..-1].join(", ")
   else
       array.join
